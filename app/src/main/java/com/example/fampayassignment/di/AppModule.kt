518f03a905935ca17d76d9e83c06ba487dcd4d5f
Haid-Faiz <haiderfaizan732@gmail.com>
2022-07-11 20:42:28 +0530

    feat: Sets up Dagger-Hilt for DI

---
+++ app/src/main/java/com/example/fampayassignment/di/AppModule.kt
@@ -0,0 +1,26 @@
+package com.example.fampayassignment.di
+
+import com.example.fampayassignment.data.CardGroupsRepository
+import com.example.fampayassignment.data.CardGroupsRepositoryImpl
+import com.example.lib.ApiClient
+import com.example.lib.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesApiClient(): ApiClient = ApiClient()
+
+    @Provides
+    @Singleton
+    fun providesApiService(apiClient: ApiClient): ApiService = apiClient
+        .retrofit.create(ApiService::class.java)
+
+}
