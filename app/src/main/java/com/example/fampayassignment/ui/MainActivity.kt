86d2c4068e1142f186f5eaf561dcfb64d38a2a7f
Haid-Faiz <haiderfaizan732@gmail.com>
2022-07-11 16:23:26 +0530

    chore: Creates lib module & sets up  project

--- app/src/main/java/com/example/fampayassignment/ui/MainActivity.kt
+++ app/src/main/java/com/example/fampayassignment/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.fampayassignment
+package com.example.fampayassignment.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fampayassignment.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
